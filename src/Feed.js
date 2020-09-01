a84c2103f1e66344a4106ba7603d1d42fd1356f2
Abdu Jemal <abduh9350@gmail.com>
2020-08-31 21:00:33 -0400

    feed added

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Feed.css'
+import Post from './Post'
 import StoryReel from './StoryReel'
 import MessageSender from './MessageSender'
 
@@ -8,6 +9,25 @@ function Feed() {
         <div className='feed'>
             <StoryReel />
             <MessageSender />
+            <Post 
+               profilePic='https://abduh9350.github.io/Portfolio/images/20200803_230735.jpg'
+               message='Wow! its working'
+               timestamp='Its 8:30pm now'
+               username='Abdu Jemal'
+               image='https://abduh9350.github.io/Portfolio/images/20200803_230735.jpg' />
+            <Post
+                profilePic='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Elon_Musk_Royal_Society.jpg/220px-Elon_Musk_Royal_Society.jpg'
+                message='Abdu I like your facebook clone'
+                timestamp='Its 8:30pm now'
+                username='Elon Musk'
+                image='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Elon_Musk_Royal_Society.jpg/220px-Elon_Musk_Royal_Society.jpg'  />
+            <Post 
+                profilePic='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Bill_Gates_2018.jpg/220px-Bill_Gates_2018.jpg'
+                message='The person who did this webpage is Pro'
+                timestamp='Its 8:30pm now'
+                username='Bill Gates'
+                image='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Bill_Gates_2018.jpg/220px-Bill_Gates_2018.jpg' />
+
             
         </div>
     )
--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,33 +1,47 @@
 import React from 'react'
+import { useState } from 'react';
 import './MessageSender.css'
 import { Avatar } from '@material-ui/core';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 function MessageSender() {
+    const[input,setInput] = useState('');
+    const[imageUrl, setImageUrl] = useState('');
+    
     const handleSubmit = (e) => {
         e.preventdefault();
     }
+
     return (
         <div className='messageSender'>
             <div className='messageSender_top'>
                 <Avatar />
                 <form>
-                    <input className='messageSender' placeholder="what's on your mind" />
-                    <input  className='messageSender' placeholder='Image URL' />
+                    <input value={input} 
+                        onChange={(e) => setInput(e.target.value)} 
+                        className='messageSender' 
+                        placeholder="what's on your mind" />
+                    <input value={imageUrl}
+                        onChange={(e) => setImageUrl(e.target.value)}
+                        className='messageSender'
+                        placeholder='Image URL' />
                     <button onClick={handleSubmit}
-                    type='submit'>
+                        type='submit'>
                         Hidden Submit
                     </button>
                 </form>
             </div>
             <div className='messageSender_bottom'>
                 <div className='messageSender_option'>
-                    <VideocamIcon style={{ color: 'red'}} />
+                    <VideocamIcon
+                     style={{ color: 'red'}} />
                     <h3>Live Video</h3>
-                    <PhotoLibraryIcon style={{ color: 'green'}} />
+                    <PhotoLibraryIcon 
+                     style={{ color: 'green'}} />
                     <h3>Photo/Video</h3>
-                    <InsertEmoticonIcon style={{ color: 'orange'}} />
+                    <InsertEmoticonIcon 
+                     style={{ color: 'orange'}} />
                     <h3>Feeling/Activity</h3>    
                 </div>
             </div>
